04c1ba8008774b7e674e0d49b33ba38006468f1f
Mohammad Ahmadi <iqbal_dyncorp@live.com>
2023-04-11 13:33:18 -0700

    added js & modified css

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+const boxes = document.querySelectorAll('.box')
 
+window.addEventListener('scroll', checkBoxes)
+
+// call checkBoxes function
+checkBoxes()
+
+function checkBoxes() {
+  const triggerBottom = (window.innerHeight / 5) * 1
+
+  boxes.forEach((box) => {
+    const boxTop = box.getBoundingClientRect().top
+
+    if (boxTop < triggerBottom) {
+      box.classList.add('show')
+    } else {
+      box.classList.remove('show')
+    }
+  })
+}
